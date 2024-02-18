a47281d70d57470e346a946666eee1e36999f780
ViditRajpurohit <viditrajpurohit27@gmail.com>
2024-02-18 12:04:47 +0530

    MY COMMIT

---
+++ script.js
@@ -0,0 +1,36 @@
+// let arr =[{
+//     name : "lambo",
+//     image : "https://images.unsplash.com/photo-1612825173281-9a193378527e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGxhbWJvcmdoaW5pfGVufDB8fDB8fHww",
+
+//     name : "porche" ,
+//     image :"https://images.unsplash.com/photo-1619844175408-c05947985e2d?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+
+//     name : "Ferari",
+//     image : "https://images.unsplash.com/photo-1592198084033-aade902d1aae?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+
+//     name : "BMW" ,
+//     image : "https://images.unsplash.com/photo-1555215695-3004980ad54e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ym13fGVufDB8fDB8fHww"
+// }]
+
+// let clutter = "";
+
+// function showTheCards (){
+//     arr.forEach(function(obj){
+
+//     // })
+
+let a = parseInt("12");
+
+let b = parseInt("12");
+
+let c = parseInt("1")
+
+if (a == b == c) console.log("equal");
+else {
+  console.log("not equal");
+}
+
+
+
+let cb = ["vidit",true,11];
+console.log(cb);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: {
+      // brand: "#ff85f4",
+      colors:{
+        'grey':'#CECCCD'
+      },
+    },
+  },
+  plugins: [],
+}
+
